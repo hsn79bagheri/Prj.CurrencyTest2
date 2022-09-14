3bc9653c2d47caaf22f105376469f406beafd65a
Bagheri <hsn79.bagheri@gmail.com>
2022-09-14 11:49:27 +0430

    LasrVersion

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using CurrencyApp.interfaces;
+using CurrencyApp.Services;
 namespace CurrencyApp
 {
     public class Startup
@@ -16,6 +17,7 @@ namespace CurrencyApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(typeof(ICurrencyConverter), typeof(CurrencyConvertor));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
